f3140d6769ea0330945fc9b048a7f555a8fd318e
guilhermeulbriki <guilhermeulbriki@gmail.com>
2020-10-15 19:53:27 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('🚀 Server started!');
 });
