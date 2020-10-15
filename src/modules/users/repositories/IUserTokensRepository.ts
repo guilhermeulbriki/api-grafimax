3e53ca0461578a2ecc4c809b7135fc5c738df4ff
guilhermeulbriki <guilhermeulbriki@gmail.com>
2020-10-15 18:59:09 -0300

    remove fakes

--- src/modules/users/repositories/IUserTokensRepository.ts
+++
@@ -1,6 +0,0 @@
-import { UserToken } from '@prisma/client';
-
-export default interface IUserTokensRepository {
-  generate(user_id: number): Promise<UserToken>;
-  findByToken(token: number): Promise<UserToken | null>;
-}
