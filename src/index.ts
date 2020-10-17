170a935f35140c718aca7d68a6e3a9488c4763c9
guilhermeulbriki <guilhermeulbriki@gmail.com>
2020-10-17 13:34:44 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,15 @@ import './shared/container';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    allowedHeaders: '*',
+    origin: ['https://grafimax.netlify.app', 'http://localhost'],
+  }),
+);
 app.use(express.json());
 app.use(routes);
 app.use(errors());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
-  );
-  next();
-});
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
