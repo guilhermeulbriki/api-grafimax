affcad94f4c90fdcf1e5fc31752e653929104bdf
guilhermeulbriki <guilhermeulbriki@gmail.com>
2020-10-17 11:24:15 -0300

    change cors

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,11 @@ import './shared/container';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://grafimax.netlify.app/',
+  }),
+);
 app.use(express.json());
 app.use(routes);
 app.use(errors());
